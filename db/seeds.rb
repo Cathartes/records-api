4d03b5e645487a7cf120760e80fbae2876a9b032
Tyler Hogan <tybot204@gmail.com>
2017-07-30 01:10:23 -0700

    Basic User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+print 'Seeding Users...'
+
+User.create! [
+  {
+    email: 'tybot204@gmail.com',
+    discord_name: 'Agthor#8442',
+    password: 'password',
+    admin: true
+  }, {
+    email: 'justinrlaforge@gmail.com',
+    discord_name: 'iDreamPixels#3186',
+    password: 'password',
+    admin: true
+  }, {
+    discord_name: 'Best 2hu#0550',
+    password: Faker::Internet.password(6, 72)
+  }, {
+    discord_name: 'Jibbers#2487',
+    password: Faker::Internet.password(6, 72)
+  }, {
+    discord_name: 'NicksPatties#4392',
+    password: Faker::Internet.password(6, 72)
+  }, {
+    discord_name: 'rebble#3220',
+    password: Faker::Internet.password(6, 72)
+  }, {
+    discord_name: 'RescuePenguin#8507',
+    password: Faker::Internet.password(6, 72)
+  }, {
+    discord_name: 'Sloth#6910',
+    password: Faker::Internet.password(6, 72)
+  }
+]
+
+puts 'done'
