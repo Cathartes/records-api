7b936d4dc6b7fce1c62a5968a781d8383003a589
Tyler Hogan <tybot204@gmail.com>
2017-08-08 14:34:57 -0700

    Fixed routing specs format

--- spec/routing/v1/sessions_routing_spec.rb
+++ spec/routing/v1/sessions_routing_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'V1 Sessions Routing', type: :routing do
   it 'routes POST "/v1/login" to SessionsController' do
-    expect(post('/v1/login')).to route_to 'v1/sessions#create', format: 'json'
+    expect(post('/v1/login')).to route_to 'v1/sessions#create', format: :json
   end
 
   it 'routes DELETE "/v1/logout" to SessionsController' do
-    expect(delete('/v1/logout')).to route_to 'v1/sessions#destroy', format: 'json'
+    expect(delete('/v1/logout')).to route_to 'v1/sessions#destroy', format: :json
   end
 end
--- spec/routing/v1/users_routing_spec.rb
+++ spec/routing/v1/users_routing_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'V1 Users Routing', type: :routing do
   it 'routes POST "/v1/users" to UsersController' do
-    expect(post('/v1/users')).to route_to 'v1/users#create', format: 'json'
+    expect(post('/v1/users')).to route_to 'v1/users#create', format: :json
   end
 
   it 'routes PATCH "/v1/users/:id" to UsersController' do
-    expect(patch('/v1/users/1')).to route_to 'v1/users#update', format: 'json', id: '1'
+    expect(patch('/v1/users/1')).to route_to 'v1/users#update', format: :json, id: '1'
   end
 end
