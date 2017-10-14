e58dd486b68bc89ed5ca76a6636396a4868df786
Tyler Hogan <tybot204@gmail.com>
2017-10-14 00:16:25 -0700

    Slight readability improvement

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,12 +2,12 @@ class GraphqlController < ApplicationController
   def execute
     skip_authorization
 
-    variables = ensure_hash(params[:variables])
-    query = params[:query]
+    variables      = ensure_hash params[:variables]
+    query          = params[:query]
     operation_name = params[:operationName]
-    context = { current_user: current_user }
+    context        = { current_user: current_user }
 
-    result = RecordsApiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = RecordsApiSchema.execute query, variables: variables, context: context, operation_name: operation_name
     render json: result
   end
 
