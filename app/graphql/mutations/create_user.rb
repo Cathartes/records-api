213c716c568c8d7a3b9c6dd607868fae1c46f48f
Tyler Hogan <tybot204@gmail.com>
2017-10-13 19:18:19 -0700

    Fixed some doc issues with create mutations

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,23 +1,16 @@
 module Mutations
-  UserInputType = GraphQL::InputObjectType.define do
-    name 'UserInputType'
-    description 'Attributes for creating a user'
-
+  class CreateUser < GraphQL::Function
     argument :discordName, !types.String, 'Discord name associated with the user', as: :discord_name
-    argument :email, types.Boolean, 'Unique email required for user login'
+    argument :email, types.String, 'Unique email required for user login'
     argument :password, types.String, 'Password required for user login'
     argument :membershipType, Types::Enums::UserMembershipTypeEnum, 'Type of member this user is'
-  end
-
-  class CreateUser < GraphQL::Function
-    argument :user, UserInputType
 
     description 'Create a single user'
     type ::Types::UserType
 
     def call(_obj, args, ctx)
       @user = User.new
-      @user.assign_attributes user_args args[:user], ctx
+      @user.assign_attributes user_args args, ctx
       @user.save
       @user
     end
