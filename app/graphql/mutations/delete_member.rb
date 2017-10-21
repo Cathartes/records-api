f16dc05d83c078c5f10d9b7f01c2edee6370836d
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-10-21 02:36:15 -0700

    Receive an integer
    Remove unused things
    
    Fix code climate

--- app/graphql/mutations/delete_member.rb
+++ app/graphql/mutations/delete_member.rb
@@ -1,23 +1,18 @@
 module Mutations
   class DeleteMember < GraphQL::Function
-    argument :id, !types.ID, 'ID of the member getting updated'
+    argument :id, !types.Int, 'ID of the member getting updated'
 
     description 'Update a member status'
     type ::Types::UserType
 
-    def call(_, args, ctx)
+    def call(_, args)
       @user = User.find args[:id]
-      if @user.participations.length > 0
-        @user.update(current_user_status: 1)
-      else
+      if @user.participations.empty?
         @user.update(current_user_status: 2)
+      else
+        @user.update(current_user_status: 1)
       end
       @user
-    end
-
-    def delete_member_args(args, ctx)
-      attrs = Pundit.policy(ctx[:current_user], @record_book).permitted_attributes
-      args.to_h.select { |k, _v| attrs.include? k.to_sym }
     end
   end
 end
